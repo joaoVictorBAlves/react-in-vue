1bd7a67739a0ca9bc01bd3597647308ad700039f
João Victor <joaovba2002@alu.ufc.br>
2023-09-13 23:13:08 -0300

    simples component react usado no vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
     <h1>Hello World in Vue.js</h1>
+    <!-- A simple component -->
+    <Button :on-click="handleClick" />
   </main>
 </template>
+
+<script>
+import Button from '../components/React/Button';
+
+export default {
+  components: {
+    Button
+  },
+  methods: {
+    handleClick() {
+      console.log('Button clicked!');
+    }
+  }
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  esbuild: {
+    jsxFactory: 'h', 
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
